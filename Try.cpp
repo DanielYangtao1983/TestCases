38a4f45fcc56282c41fb62223e2e76bc96b19f47
Dainelyangtao <daniel@localhost.localdomain>
2018-06-05 21:06:18 +0530

    add sth

--- Try.cpp
+++ Try.cpp
@@ -1,10 +1,27 @@
 #include <iostream>
-#include <string>
+#include <cstring>
+#include <algorithm>
 using namespace std;
+
+#define STR_LEN  10000
 
 int main(int, char**)
 {
-	string a = "Hello world!";
-	cout<< a.length()<<endl;
+	char a [STR_LEN];
+
+	for(int i = 0; i < STR_LEN; ++i)
+		a[i] = 'A' + (int)abs(random())%26;
+
+	a[STR_LEN] = '\0';
+
+//	sort(a,a+strlen(a));
+//	cout<< "after:"<<endl<<a <<endl;
+
+
+	for(int i = 0; i < STR_LEN; ++i)
+	{
+		int cnt = count(a,a+STR_LEN,a[i]);
+		cout<<i<<";"<<a[i] << ";"<<cnt<<";"<<endl;
+	}
 	return 0;
 }
